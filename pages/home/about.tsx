3ec2cf881a2ca32bfeea7afa8fdb79a46f52956f
Jessica <guillotjessicab@gmail.com>
2024-08-29 14:26:47 +0200

    add tailwind

--- pages/home/about.tsx
+++ pages/home/about.tsx
@@ -1,12 +1,11 @@
-import styles from "../../styles/Home.module.css";
 import Link from "next/link";
 
 export default function About() {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
+    <div>
+      <main>
         <h1>About Page</h1>
-        <p className={styles.description}>
+        <p>
           <Link href="/home">&larr; Go Back</Link>
         </p>
       </main>
